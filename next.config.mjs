ce1c20572c32fa4482a9e4f6252fb1dd4d75fedf
szelaag <business.szelag@gmail.com>
2025-08-16 18:53:51 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { formats: ['image/avif','image/webp'] },
-  experimental: { optimizeCss: true },
+  images: { formats: ['image/avif', 'image/webp'] },
+  // WAŻNE: nie używamy optimizeCss, bo wymaga paczki "critters"
+  // Usuń całe "experimental" albo zostaw puste:
+  experimental: {},
   poweredByHeader: false,
   reactStrictMode: true,
   compress: true,
